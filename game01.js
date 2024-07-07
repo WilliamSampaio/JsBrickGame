5554b21960f3c002d9465baa9104fe9db4fef742
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-07-07 12:01:21 -0400

    update

--- game01.js
+++ game01.js
@@ -4,25 +4,25 @@ const ctx = canvas.getContext('2d')
 function resize() {
     if (innerWidth > innerHeight) {
         canvas.height = innerHeight * 0.97
-        canvas.width = ((innerHeight * 0.97) / 4) * 3
+        canvas.width = ((innerHeight * 0.97) / 16) * 9
     }
     if (innerWidth < innerHeight) {
         canvas.width = innerWidth * 0.97
-        canvas.height = (canvas.width / 3) * 4
+        canvas.height = ((innerWidth * 0.97) / 9) * 16
     }
 }
 
-function drawBlock(x, y, cellSize) {
+function drawBlock(posX, posY, boardX, boardY, cellSize) {
     ctx.fillStyle = 'orange'
     ctx.fillRect(
-        (cellSize * x) + (cellSize * 0.025),
-        (cellSize * y) + (cellSize * 0.025),
+        posX + (cellSize * boardX) + (cellSize * 0.025),
+        posY + (cellSize * boardY) + (cellSize * 0.025),
         cellSize * 0.95,
         cellSize * 0.95
     )
     ctx.clearRect(
-        (cellSize * x) + (cellSize * 0.2),
-        (cellSize * y) + (cellSize * 0.2),
+        posX + (cellSize * boardX) + (cellSize * 0.2),
+        posY + (cellSize * boardY) + (cellSize * 0.2),
         cellSize * 0.6,
         cellSize * 0.6
     )
@@ -35,16 +35,53 @@ class Position {
     }
 }
 
+class Button {
+    constructor(text, x, y, radius) {
+        this.text = text
+        this.x = x
+        this.y = y
+        this.radius = radius
+    }
+
+    inBounds(posX, posY) {
+        let x = this.x - this.radius / 2
+        let y = this.y - this.radius / 2
+        let w = this.radius * 2
+        let h = this.radius * 2
+        if (posX >= x && posX < x + w && posY >= y && posY < y + h) {
+            return true
+        }
+        return false
+    }
+
+    draw() {
+        ctx.strokeStyle = 'lightblue'
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        ctx.stroke()
+        ctx.textBaseline = 'middle'
+        ctx.textAlign = 'center'
+        ctx.fillStyle = 'lightblue'
+        ctx.fillText(this.text, this.x, this.y);
+    }
+}
+
 class Gun {
     constructor(boardH) {
         this.pos = new Position(1, boardH - 1)
     }
 
-    draw(cellSize) {
-        drawBlock(this.pos.x, this.pos.y - 1, cellSize)
-        drawBlock(this.pos.x - 1, this.pos.y, cellSize)
-        drawBlock(this.pos.x, this.pos.y, cellSize)
-        drawBlock(this.pos.x + 1, this.pos.y, cellSize)
+    draw(posX, posY, cellSize) {
+        let xl = this.pos.x - 1
+        let xr = this.pos.x + 1
+        drawBlock(posX, posY, this.pos.x, this.pos.y - 1, cellSize)
+        if (xl >= 0) {
+            drawBlock(posX, posY, this.pos.x - 1, this.pos.y, cellSize)
+        }
+        drawBlock(posX, posY, this.pos.x, this.pos.y, cellSize)
+        if (xr < 10) {
+            drawBlock(posX, posY, this.pos.x + 1, this.pos.y, cellSize)
+        }
     }
 }
 
@@ -58,8 +95,8 @@ class Bullet {
         this.pos.y += this.velY
     }
 
-    draw(cellSize) {
-        drawBlock(this.pos.x, this.pos.y, cellSize)
+    draw(posX, posY, cellSize) {
+        drawBlock(posX, posY, this.pos.x, this.pos.y, cellSize)
     }
 }
 
@@ -153,20 +190,19 @@ class Wall {
         return false
     }
 
-    draw(cellSize) {
+    draw(posX, posY, cellSize) {
         for (let y = 0; y < this.rows.length; y++) {
             for (let x = 0; x < this.rows[y].length; x++) {
-                if (this.rows[y][x] == 1) drawBlock(x, y, cellSize)
+                if (this.rows[y][x] == 1) drawBlock(posX, posY, x, y, cellSize)
             }
         }
     }
 }
 
-let screenWidth = null
-let screenHeight = null
+let screenPosX, screenPosY = null
+let screenWidth, screenHeight = null
 let cellSize = null
-let gameBoardWidth = null
-let gameBoardHeight = null
+let gameBoardWidth, gameBoardHeight = null
 let score = null
 let gun = null
 let shots = []
@@ -175,10 +211,14 @@ let paused = false
 let gameOver = false
 let lives = 4
 
+let buttons = []
+
 function resizeGame() {
-    screenWidth = canvas.width
-    screenHeight = canvas.height
-    cellSize = canvas.height / 20
+    screenWidth = canvas.width * 0.7
+    screenHeight = (screenWidth / 3) * 4
+    screenPosX = (canvas.width - screenWidth) / 2
+    screenPosY = (canvas.width - screenWidth) / 2
+    cellSize = screenHeight / 20
 }
 
 function init() {
@@ -191,6 +231,12 @@ function init() {
     wall = new Wall()
     paused = false
     gameOver = false
+
+    buttons.push(new Button('SPACE', canvas.width * 0.85, (canvas.height / 16) * 13, 40))
+    buttons.push(new Button('<', canvas.width * .15, (canvas.height / 16) * 13, 20))
+    buttons.push(new Button('>', canvas.width * .35, (canvas.height / 16) * 13, 20))
+    buttons.push(new Button('^', canvas.width * .25, (canvas.height / 16) * 12, 20))
+    buttons.push(new Button('v', canvas.width * .25, (canvas.height / 16) * 14, 20))
 }
 
 function update() {
@@ -229,46 +275,46 @@ function update() {
 
 function draw() {
 
+    ctx.clearRect(0, 0, screenWidth, screenHeight)
+
     ctx.fillStyle = '#00ff00'
-    ctx.fillRect(0, 0, cellSize * gameBoardWidth, cellSize * gameBoardHeight)
-    ctx.clearRect(1, 1, (cellSize * gameBoardWidth) - 2, (cellSize * gameBoardHeight) - 2)
+    ctx.fillRect(screenPosX, screenPosY, screenWidth, screenHeight)
+    ctx.fillStyle = '#000000'
+    ctx.fillRect(screenPosX + 1, screenPosY + 1, screenWidth - 2, screenHeight - 2)
+    ctx.fillStyle = '#00ff00'
+    ctx.fillRect(screenPosX, screenPosY, cellSize * gameBoardWidth, cellSize * gameBoardHeight)
+    ctx.fillStyle = '#000000'
+    ctx.fillRect(screenPosX + 1, screenPosY + 1, (cellSize * gameBoardWidth) - 2, (cellSize * gameBoardHeight) - 2)
 
-    gun.draw(cellSize)
+    gun.draw(screenPosX, screenPosY, cellSize)
 
-    wall.draw(cellSize)
+    wall.draw(screenPosX, screenPosY, cellSize)
 
     shots.forEach(b => {
-        b.draw(cellSize)
+        b.draw(screenPosX, screenPosY, cellSize)
     })
 
-    ctx.clearRect(
-        gameBoardWidth * cellSize,
-        0,
-        screenWidth - (gameBoardWidth * cellSize),
-        screenHeight
-    )
-
     for (let l = 0; l < lives; l++) {
-        let x = cellSize * (gameBoardWidth + (cellSize * 2))
-        console.log(cellSize)
-        drawBlock(gameBoardWidth + 2, 4 + l, cellSize)
+        drawBlock(screenPosX, screenPosY, gameBoardWidth + 2, 4 + l, cellSize)
     }
 
     ctx.font = `${cellSize}px Monospace`
     ctx.fillStyle = 'green'
-    ctx.fillText(('000000' + score).slice(-6), screenWidth - (cellSize * 3.8), cellSize)
-    ctx.fillText('SCORE', screenWidth - (cellSize * 3.2), cellSize * 2)
+    ctx.fillText(('000000' + score).slice(-6), (screenPosX + screenWidth) - (cellSize * 3.8), screenPosY + cellSize)
+    ctx.fillText('SCORE', (screenPosX + screenWidth) - (cellSize * 3.2), screenPosY + cellSize * 2)
 
     if (paused) {
         ctx.fillStyle = 'yellow'
-        ctx.fillText('PAUSE', screenWidth - (cellSize * 3.2), (gameBoardHeight - 4) * cellSize)
+        ctx.fillText('PAUSE', (screenPosX + screenWidth) - (cellSize * 3.2), screenPosY + (gameBoardHeight - 4) * cellSize)
     }
 
     if (gameOver) {
         ctx.fillStyle = 'red'
-        ctx.fillText('GAME', screenWidth - (cellSize * 3.2), (gameBoardHeight - 2) * cellSize)
-        ctx.fillText('OVER', screenWidth - (cellSize * 3.2), (gameBoardHeight - 1) * cellSize)
+        ctx.fillText('GAME', (screenPosX + screenWidth) - (cellSize * 3.2), screenPosY + (gameBoardHeight - 2) * cellSize)
+        ctx.fillText('OVER', (screenPosX + screenWidth) - (cellSize * 3.2), screenPosY + (gameBoardHeight - 1) * cellSize)
     }
+
+    buttons.forEach(btn => btn.draw())
 }
 
 function move(x) {
@@ -279,11 +325,13 @@ function move(x) {
     if (x > 0 && gun.pos.x < gameBoardWidth - 1) {
         gun.pos.x += x
     }
+    navigator.vibrate([200])
 }
 
 function shoot() {
     if (gameOver || paused) return
     shots.push(new Bullet(gun.pos.x, gun.pos.y - 1))
+    navigator.vibrate([200])
 }
 
 function handleKey(e) {
@@ -310,6 +358,28 @@ function handleKey(e) {
 }
 
 addEventListener('keydown', handleKey)
+
+function handleTouch(e) {
+    let touch = e.touches[0]
+    let rect = canvas.getBoundingClientRect()
+    buttons.forEach((btn, index) => {
+        if (btn.inBounds(touch.clientX - rect.left, touch.clientY - rect.top)) {
+            switch (index) {
+                case 0:
+                    shoot()
+                    break
+                case 1:
+                    move(-1)
+                    break
+                case 2:
+                    move(1)
+                    break
+            }
+        }
+    })
+}
+
+addEventListener('touchstart', handleTouch)
 
 addEventListener('resize', () => {
     resize()
